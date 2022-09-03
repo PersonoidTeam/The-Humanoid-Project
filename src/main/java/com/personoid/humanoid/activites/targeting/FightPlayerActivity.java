5a074ab6f1033f58b557097d412fe94ab1f7962c
DefineDoddy <definedoddyyt@gmail.com>
2022-09-03 21:28:51 +0100

    attack damage + crit effect

--- src/main/java/com/personoid/humanoid/activites/targeting/FightPlayerActivity.java
+++ src/main/java/com/personoid/humanoid/activites/targeting/FightPlayerActivity.java
@@ -1,18 +1,27 @@
 package com.personoid.humanoid.activites.targeting;
 
 import com.google.common.collect.Multimap;
+import com.personoid.api.activities.GoToLocationActivity;
 import com.personoid.api.ai.activity.Activity;
 import com.personoid.api.ai.activity.ActivityType;
 import com.personoid.api.ai.looking.Target;
+import com.personoid.api.ai.movement.MovementType;
 import com.personoid.api.utils.Result;
 import com.personoid.api.utils.packet.ReflectionUtils;
 import com.personoid.api.utils.types.HandEnum;
 import com.personoid.api.utils.types.Priority;
+import com.personoid.humanoid.Humanoid;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.protocol.game.ClientboundLevelParticlesPacket;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -31,9 +40,11 @@ public class FightPlayerActivity extends Activity {
     @Override
     public void onStart(StartType startType) {
         getNPC().getLookController().addTarget("fight_target", new Target(player, Priority.HIGHEST));
-        getNPC().getNPCInventory().addItem(new ItemStack(Material.NETHERITE_SWORD));
-        switchToItem(Material.NETHERITE_SWORD);
+        getNPC().getNPCInventory().addItem(new ItemStack(Material.NETHERITE_AXE));
+        switchToItem(Material.NETHERITE_AXE);
         attackCooldown = getAttackSpeed(getNPC().getNPCInventory().getSelectedItem());
+        player.hidePlayer(Humanoid.getPlugin(), getNPC().getEntity());
+        player.showPlayer(Humanoid.getPlugin(), getNPC().getEntity());
     }
 
     private int switchToItem(Material material) {
@@ -71,10 +82,45 @@ public class FightPlayerActivity extends Activity {
     @Override
     public void onUpdate() {
         if (checkWinStatus()) return;
+        run(new GoToLocationActivity(player.getLocation(), MovementType.SPRINTING));
         if (attackCooldown <= 0) {
-            getNPC().swingHand(HandEnum.RIGHT);
+            getNPC().getEntity().resetPlayerTime();
+            if (getNPC().getLocation().distance(player.getLocation()) < 5) {
+                getNPC().swingHand(HandEnum.RIGHT);
+/*                ServerPlayer npcServerPlayer = ((CraftPlayer)getNPC().getEntity()).getHandle();
+                ServerPlayer playerServerPlayer = ((CraftPlayer)player).getHandle();
+
+                ChunkMap tracker = ((ServerLevel)playerServerPlayer.level).getChunkSource().chunkMap;
+                ChunkMap.TrackedEntity entry = tracker.entityMap.get(getNPC().getEntity().getEntityId());
+                if (entry != null) {
+                    entry.removePlayer(npcServerPlayer);
+                    Packets.removePlayer(getNPC().getEntity()).send();
+                    entry.updatePlayer(npcServerPlayer);
+                    Packets.addPlayer(getNPC().getEntity()).send();
+                }*/
+/*                    Class<?> nmsPlayerClass = npc.getClass().getMethod("getHandle").invoke(npc).getClass();
+                    Bukkit.broadcastMessage("NMS player: " + nmsPlayerClass);
+                    MethodHandle handle = MethodHandles.lookup().findSpecial()
+                    Class<?> attribute = nmsPlayerClass.getMethod("a").invoke(Attributes.ATTACK_DAMAGE).getClass();
+                    Bukkit.broadcastMessage("Attribute: " + attribute);
+                    double damage = (double) attribute.getMethod("f").invoke(attribute);*/
+                //double damage = getNPC().getEntity().getAttribute(org.bukkit.attribute.Attribute.GENERIC_ATTACK_DAMAGE).getValue();
+                getNPC().getEntity().attack(player);
+                double x = player.getLocation().getX();
+                double y = player.getLocation().getY() + 2;
+                double z = player.getLocation().getZ();
+                ClientboundLevelParticlesPacket packet = new ClientboundLevelParticlesPacket(ParticleTypes.CRIT, false, x, y, z, .5F, .4F, .5F, 0, 15);
+                for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                    ServerPlayer serverPlayer = ((CraftPlayer)onlinePlayer).getHandle();
+                    serverPlayer.connection.send(packet);
+                }
+                getNPC().getLocation().getWorld().playSound(getNPC().getLocation(), Sound.ENTITY_PLAYER_ATTACK_CRIT, 1, 1);
+            }
             attackCooldown = getAttackSpeed(getNPC().getNPCInventory().getSelectedItem());
-        } else attackCooldown--;
+        } else {
+            if (attackCooldown < 10) getNPC().getMoveController().jump();
+            attackCooldown--;
+        }
     }
 
     @Override
